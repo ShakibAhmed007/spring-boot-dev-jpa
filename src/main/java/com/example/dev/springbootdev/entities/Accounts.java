b1398990b4a10bc58cab6efc75ba4766a008b00a
unknown <shakibahmed09112017@gmail.com>
2022-10-18 15:47:38 +0600

    add lombok

--- src/main/java/com/example/dev/springbootdev/entities/Accounts.java
+++ src/main/java/com/example/dev/springbootdev/entities/Accounts.java
@@ -1,11 +1,20 @@
 package com.example.dev.springbootdev.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name="accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Accounts {
 
     @Id
@@ -32,6 +41,10 @@ public class Accounts {
     @NotNull
     private String userRole;
 
+    @Column(name = "status_id")
+    @NotNull
+    private Integer statusId;
+
     @Column(name = "created_on")
     @NotNull
     private LocalDateTime createdOn;
@@ -42,107 +55,4 @@ public class Accounts {
 
     @Column(name = "last_login")
     private LocalDateTime lastLogin;
-
-    public Accounts() {
-
-    }
-
-    public Accounts(Long id, String userName, String password, String userEmail, Integer userAge, String userRole, LocalDateTime createdOn, LocalDateTime updatedOn, LocalDateTime lastLogin) {
-        this.id = id;
-        this.userName = userName;
-        this.password = password;
-        this.userEmail = userEmail;
-        this.userAge = userAge;
-        this.userRole = userRole;
-        this.createdOn = createdOn;
-        this.updatedOn = updatedOn;
-        this.lastLogin = lastLogin;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public Integer getUserAge() {
-        return userAge;
-    }
-
-    public void setUserAge(Integer userAge) {
-        this.userAge = userAge;
-    }
-
-    public String getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(String userRole) {
-        this.userRole = userRole;
-    }
-
-    public LocalDateTime getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(LocalDateTime createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public LocalDateTime getUpdatedOn() {
-        return updatedOn;
-    }
-
-    public void setUpdatedOn(LocalDateTime updatedOn) {
-        this.updatedOn = updatedOn;
-    }
-
-    public LocalDateTime getLastLogin() {
-        return lastLogin;
-    }
-
-    public void setLastLogin(LocalDateTime lastLogin) {
-        this.lastLogin = lastLogin;
-    }
-
-    @Override
-    public String toString() {
-        return "Accounts{" +
-                "id=" + id +
-                ", userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                ", userEmail='" + userEmail + '\'' +
-                ", userAge=" + userAge +
-                ", userRole='" + userRole + '\'' +
-                ", createdOn=" + createdOn +
-                ", updatedOn=" + updatedOn +
-                ", lastLogin=" + lastLogin +
-                '}';
-    }
 }
--- src/main/java/com/example/dev/springbootdev/entities/Book.java
+++ src/main/java/com/example/dev/springbootdev/entities/Book.java
@@ -1,10 +1,20 @@
 package com.example.dev.springbootdev.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name="book")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id
@@ -12,30 +22,43 @@ public class Book {
     private Long id;
 
     @Column(name="book_name")
+    @NotNull
     private String bookName;
 
     @Column(name="author_name")
+    @NotNull
     private String authorName;
 
     @Column(name="genre")
+    @NotNull
     private String genre;
 
     @Column(name="price")
+    @NotNull
     private Double price;
 
     @Column(name="page_count")
+    @NotNull
     private Integer pageCount;
 
     @Column(name="description")
+    @NotNull
     private String description;
 
+    @Column(name = "status_id")
+    @NotNull
+    private Integer statusId;
+
     @Column(name="published_date")
+    @NotNull
     private LocalDateTime publishedDate;
 
     @Column(name="created_on")
+    @NotNull
     private LocalDateTime createdOn;
 
     @Column(name="updated_on")
+    @NotNull
     private LocalDateTime updatedOn;
 
 }
