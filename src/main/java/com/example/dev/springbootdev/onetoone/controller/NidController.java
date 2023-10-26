1f7af22411b78be62776f18220a8a088619baf40
unknown <shakibahmed09112017@gmail.com>
2023-10-27 00:51:27 +0600

    nid getall and getbyid add

--- src/main/java/com/example/dev/springbootdev/onetoone/controller/NidController.java
+++ src/main/java/com/example/dev/springbootdev/onetoone/controller/NidController.java
@@ -7,17 +7,35 @@ import com.example.dev.springbootdev.onetoone.dto.NidDto;
 import com.example.dev.springbootdev.onetoone.service.NidService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController("/nid")
 public class NidController {
 
     @Autowired
     private NidService nidService;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<CustomResponse<List<Nid>>> getAll(){
+        List<Nid> nids = nidService.getAll();
+        CustomResponse<List<Nid>> response = CustomResponseUtil.createResponse(200, nids, "/GetAll");
+        response.setMessage("Successfull");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<CustomResponse<NidDto>> getById(@PathParam("id") Long id){
+        NidDto nid = nidService.getById(id);
+        CustomResponse<NidDto> response = CustomResponseUtil.createResponse(200, nid, "/GetById");
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/save")
     public ResponseEntity<CustomResponse<Nid>> save(@Valid @RequestBody NidDto nidDto){
--- src/main/java/com/example/dev/springbootdev/onetoone/service/NidService.java
+++ src/main/java/com/example/dev/springbootdev/onetoone/service/NidService.java
@@ -9,6 +9,7 @@ import com.example.dev.springbootdev.onetoone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +20,27 @@ public class NidService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<Nid> getAll() {
+        return nidRepository.findAll();
+    }
+
+    public NidDto getById(Long id) {
+        Optional<Nid> nidOptional = nidRepository.findById(id);
+        if(nidOptional.isPresent()){
+            NidDto nidDto = new NidDto();
+            nidDto.setId(nidOptional.get().getId());
+            nidDto.setNumber(nidOptional.get().getNumber());
+            nidDto.setAddress(nidOptional.get().getAddress());
+            nidDto.setFatherName(nidOptional.get().getFatherName());
+            nidDto.setMotherName(nidOptional.get().getMotherName());
+            nidDto.setStatus(nidOptional.get().getStatus());
+            return nidDto;
+        } else {
+            throw new CustomException(404, "Nid not found with given id"+ id);
+        }
+    }
+
     public Nid save(NidDto nidDto) {
         Optional<User> userOptional = userRepository.findById(nidDto.getUserId());
         if(userOptional.isPresent()){
